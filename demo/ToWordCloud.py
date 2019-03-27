7baba176f77850cf817cc2a745dc46ead54ac79d
xpf <xpf@junquan.com.cn>
2019-03-27 15:19:58 +0800

    python 生成词云

---
+++ demo/ToWordCloud.py
@@ -0,0 +1,45 @@
+#coding=gbk
+#python …˙≥…¥ ‘∆
+
+
+#ºÚµ•¥ ‘∆
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import jieba
+
+text_from_file_with_apath = open('D:/thisme_D/Python/python_study/demo/hello.txt',encoding='utf-8').read()
+
+wordlist_after_jieba = jieba.cut(text_from_file_with_apath, cut_all=True)
+wl_space_split = " ".join(wordlist_after_jieba)
+
+my_wordcloud = WordCloud(background_color="white",width=1000,height=860, font_path="C:\\Windows\\Fonts\\STFANGSO.ttf").generate(wl_space_split)
+
+plt.imshow(my_wordcloud)
+plt.axis("off")
+plt.show()
+
+#¥ ‘∆Demo
+
+import jieba.analyse
+from PIL import Image,ImageSequence
+import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud,ImageColorGenerator
+
+article= open('D:/thisme_D/Python/python_study/demo/hello.txt',encoding='utf-8').read()
+
+result=jieba.analyse.textrank(article,topK=50,withWeight=True)
+keywords = dict()
+for i in result:
+    keywords[i[0]]=i[1]
+print(keywords)
+
+image= Image.open('D:/thisme_D/Python/python_study/demo/timg.jpg')
+graph = np.array(image)
+wc = WordCloud(font_path='C:/Windows/fonts/simhei.ttf',background_color='white',max_words=50,mask=graph,width=700,height=1244)
+wc.generate_from_frequencies(keywords)
+image_color = ImageColorGenerator(graph)
+plt.imshow(wc)
+plt.imshow(wc.recolor(color_func=image_color))
+plt.axis("off")
+plt.show()
--- workspaceDemo/fastai_image.py
+++ workspaceDemo/fastai_image.py
@@ -1,1 +1,86 @@
 #coding=utf-8
+#imageclassification
+
+from fastai import *
+from fastai.vision import *
+
+folder = 'black'
+file = 'urls_black.txt'
+
+folder = 'teddys'
+file = 'urls_teddys.txt'
+
+folder = 'grizzly'
+file = 'urls_grizzly.txt'
+
+#download_images
+
+path = Path('data/bears')
+dest = path/folder
+dest.mkdir(parents=True, exist_ok=True)
+
+classes = ['teddys','grizzly','black']
+download_images(path/file, dest, max_pics=200)
+
+for c in classes:
+    print(c)
+    verify_images(path/c, delete=True, max_workers=8)
+
+np.random.seed(42)
+data = ImageDataBunch.from_folder(path, train=".", valid_pct=0.2,
+                                  ds_tfms=get_transforms(), size=224, num_workers=4).normalize(imagenet_stats)
+data.classes
+data.show_batch(rows=3, figsize=(7,8))
+
+data.classes, data.c, len(data.train_ds), len(data.valid_ds)
+
+#Train Model
+
+learn = create_cnn(data, models.resnet34, metrics=error_rate)
+learn.fit_one_cycle(4)
+learn.save('stage-1')
+learn.unfreeze()
+learn.lr_find()
+learn.recorder.plot()
+learn.fit_one_cycle(2, max_lr=slice(3e-5,3e-4))
+learn.save('stage-2')
+
+#interpretation
+iterp.load('stage-2')
+interp = ClassificationInterpretation.from_learner(learn)
+interp.plot_confusion_matrix()
+
+#cleaning up  干扰的数据或者无用的数据清理
+from fastai.widgets import *
+
+losses,idxs = interp.top_losses()
+top_loss_paths = data.valid_ds.x[idxs]
+
+fd = FileDeleter(file_paths=top_loss_paths)
+
+#Putting your model in production   生产测试
+data.classes
+
+#如果不是使用GPU，则进行下面配置。反之则免之
+fastai.defaults.device = torch.device('cpu')
+img = open_image(path/'black'/'0000021.jpg')
+img
+
+classes = ['black', 'grizzly', 'teddys']
+data2 = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=224).normalize(imagenet_stats)
+learn = create_cnn(data2, models.resnet34)
+learn.load('stage-2')
+
+pred_class,pred_idx,outputs = learn.predict(img)
+pred_class
+
+learn = create_cnn(data, models.resnet34, metrics=error_rate)
+learn.fit_one_cycle(1, max_lr=0.5)
+
+learn = create_cnn(data, models.resnet34, metrics=error_rate0)
+learn.fit_one_cycle(5, max_lr=1e-5)
+learn.recorder.plot_losses()
+
+learn = create_cnn(data, models.resnet34, metrics=error_rate)
+learn.fit_one_cycle(1)
+
