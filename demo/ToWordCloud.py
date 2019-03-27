4d825707821a3c435b5d5a87130a6c9f2f638c7e
xpf <xpf@junquan.com.cn>
2019-03-27 15:22:45 +0800

    注

--- demo/ToWordCloud.py
+++ demo/ToWordCloud.py
@@ -7,13 +7,17 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import jieba
 
+#加载文件内容
 text_from_file_with_apath = open('D:/thisme_D/Python/python_study/demo/hello.txt',encoding='utf-8').read()
 
+#结巴分词器分词
 wordlist_after_jieba = jieba.cut(text_from_file_with_apath, cut_all=True)
 wl_space_split = " ".join(wordlist_after_jieba)
 
+#云图的生成
 my_wordcloud = WordCloud(background_color="white",width=1000,height=860, font_path="C:\\Windows\\Fonts\\STFANGSO.ttf").generate(wl_space_split)
 
+#显示
 plt.imshow(my_wordcloud)
 plt.axis("off")
 plt.show()
@@ -34,6 +38,7 @@ for i in result:
     keywords[i[0]]=i[1]
 print(keywords)
 
+#获取原型图片
 image= Image.open('D:/thisme_D/Python/python_study/demo/timg.jpg')
 graph = np.array(image)
 wc = WordCloud(font_path='C:/Windows/fonts/simhei.ttf',background_color='white',max_words=50,mask=graph,width=700,height=1244)
